1708b4a626829503c8ddbfe3fed8e123701650de
Ben Ferry <leigero@mgail.com>
2025-08-01 01:17:20 -0400

    fixed supabase url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://abjwliydsnuolrypqfbi.supabase.co',
+  supabaseUrl: 'e4510e3f6314b2f051a02b203fbb4dc500ecc06c80452001db42a0e7717e166a',
   supabaseAnonKey: '65e67229b5fb30f59910caf52981f8d9904e8be441b352b0920647167aa904ef',
 };
